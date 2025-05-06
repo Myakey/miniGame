2b2b4f11692ca9b9711bd3699b6abf0b8b7faa1f
hanzue <rifaihafis9@gmail.com>
2025-05-06 18:48:13 +0700

    adding characters and background vn to image folder

--- src/routes/MainMenu.jsx
+++ src/routes/MainMenu.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 import Button from '../components/UI/Buttons'
 import titleScreen from '../assets/image/titleScreen/title.png'
+import titleBackground from '../assets/image/titleScreen/titleScreen.jpg'
 import Modal from "../components/UI/ModalBox"
 import { useNavigate } from 'react-router-dom';
 
 function MainMenu(){
     let navigate = useNavigate();
     return(
-       <>
-        <div className="m-20 justify-self-center">
-            <img src={titleScreen} className="h-50"/>
-            <div className="flex flex-col gap-10 items-center mt-20">
-                <Button text="Hi!"/>
-                <Modal 
-                text= 
-                    <div className="flex flex-row justify-between">
-                        <Button text="Char Select" onClick={()=>{navigate('/')}}/>
-                        <Button text="InGame" onClick={()=>{navigate('/inGame')}}/>
-                        <Button text="VN" onClick={()=>{navigate('/vn')}}/>
-                    </div>
-                button = "Debug Mode"/>
+       <div
+            className="main-menu-container"
+            style={{
+                backgroundImage: `url(${titleBackground})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '100vh',
+                width: '100vw',
+            }}
+       >
+            <div className="m-20 justify-self-center">
+                <img src={titleScreen} className="h-50"/>
+                <div className="flex flex-col gap-10 items-center mt-20">
+                    <Button text="Hi!"/>
+                    <Modal 
+                    text= 
+                        <div className="flex flex-row justify-between">
+                            <Button text="Char Select" onClick={()=>{navigate('/')}}/>
+                            <Button text="InGame" onClick={()=>{navigate('/inGame')}}/>
+                            <Button text="VN" onClick={()=>{navigate('/vn')}}/>
+                        </div>
+                    button = "Debug Mode"
+                    />
+                </div>
             </div>
-        </div>
-       </>
+       </div>
     )
 }
 
