0c8bb4a95d2b5c3595b7f2d551430b607c73c278
hanzue <rifaihafis9@gmail.com>
2025-05-16 09:49:38 +0700

    initialize VN

--- src/components/VN/dialogueData.js
+++ src/components/VN/dialogueData.js
@@ -1,46 +1,233 @@
 import {characterSprites, backgroundImages } from "./imageMap";
 
-export const dialogueData = [
+export const actData = {
+
+act1: [
     {
         speaker: "Reimu",
         text: "hi! nice to meet you",
-        sprite: "reimu",
-        position: "right",
-        background: "shrine"
+        background: "shrine",
+        characters: [
+            { name: "Reimu", sprite: "reimu", position: "right" },
+            { name: "Sakuya", sprite: "sakuya", position: "left" }
+        ]
     },
     {
         speaker: "Sakuya",
         text: "hi, nice to meet you too",
-        sprite: "sakuya",
-        position: "left",
-        background: "shrine"
+        background: "shrine",
+        characters: [
+            { name: "Reimu", sprite: "reimu", position: "right" },
+            { name: "Sakuya", sprite: "sakuya", position: "left" }
+        ]
     },
     {
         speaker: "Reimu",
         text: "let's go to my house",
-        sprite:"reimu2",
-        position: "right",
-        background: "shrine"
+        background: "shrine",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya2", position: "left" }
+        ]
     },
     {
         speaker: "Sakuya",
         text: "alright then",
-        sprite:"sakuya2",
-        position: "left",
-        background: "shrine"
+        background: "shrine",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya2", position: "left" }
+        ]
     },
     {
         speaker: "Reimu",
         text: "welcome to my house",
-        sprite:"reimu2",
-        position: "right",
-        background: "house"
+        background: "house",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya2", position: "left" }
+        ]
     },
     {
         speaker: "Sakuya",
         text: "it's a good house",
-        sprite:"sakuya",
-        position: "left",
-        background: "house"
+        background: "house",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya", position: "left" }
+        ]
     },
-];
+],
+
+act2: [
+    {
+        speaker: "Flandre",
+        text: "hi! nice to meet you",
+        background: "blokM",
+        characters: [
+            { name: "Flandre", sprite: "flandre", position: "right" },
+            { name: "Nitori", sprite: "nitori", position: "left" }
+        ]
+    },
+    {
+        speaker: "Nitori",
+        text: "hi! nice to meet you too",
+        background: "blokM",
+        characters: [
+            { name: "Flandre", sprite: "flandre", position: "right" },
+            { name: "Nitori", sprite: "nitori", position: "left" }
+        ]
+    },
+    {
+        speaker: "Flandre",
+        text: "It's a nice day today",
+        background: "blokM",
+        characters: [
+            { name: "Flandre", sprite: "flandre3", position: "right" },
+            { name: "Nitori", sprite: "nitori3", position: "left" }
+        ]
+    },
+    {
+        speaker: "Nitori",
+        text: "Yes pretty nice",
+        background: "blokM",
+        characters: [
+            { name: "Flandre", sprite: "flandre2", position: "right" },
+            { name: "Nitori", sprite: "nitori2", position: "left" }
+        ]
+    },
+    {
+        speaker: "Flandre",
+        text: "Here too",
+        background: "tangerang",
+        characters: [
+            { name: "Flandre", sprite: "flandre3", position: "right" },
+            { name: "Nitori", sprite: "nitori4", position: "left" }
+        ]
+    },
+    {
+        speaker: "Nitori",
+        text: "Yes",
+        background: "tangerang",
+        characters: [
+            { name: "Flandre", sprite: "flandre", position: "right" },
+            { name: "Nitori", sprite: "nitori2", position: "left" }
+        ]
+    },
+],
+
+act3: [
+    {
+        speaker: "Reimu",
+        text: "hi! nice to meet you",
+        background: "houseNoon",
+        characters: [
+            { name: "Reimu", sprite: "reimu", position: "right" },
+            { name: "Sakuya", sprite: "sakuya", position: "left" }
+        ]
+    },
+    {
+        speaker: "Sakuya",
+        text: "hi, nice to meet you too",
+        background: "houseNoon",
+        characters: [
+            { name: "Reimu", sprite: "reimu", position: "right" },
+            { name: "Sakuya", sprite: "sakuya", position: "left" }
+        ]
+    },
+    {
+        speaker: "Reimu",
+        text: "let's go to my house",
+        background: "houseNoon",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya2", position: "left" }
+        ]
+    },
+    {
+        speaker: "Sakuya",
+        text: "alright then",
+        background: "houseNoon",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya2", position: "left" }
+        ]
+    },
+    {
+        speaker: "Reimu",
+        text: "welcome to my house",
+        background: "tangNoon",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya2", position: "left" }
+        ]
+    },
+    {
+        speaker: "Sakuya",
+        text: "it's a good house",
+        background: "tangNoon",
+        characters: [
+            { name: "Reimu", sprite: "reimu2", position: "right" },
+            { name: "Sakuya", sprite: "sakuya", position: "left" }
+        ]
+    },
+],
+
+act4: [
+    {
+        speaker: "Flandre",
+        text: "hi! nice to meet you",
+        background: "tangerang",
+        characters: [
+            { name: "Flandre", sprite: "flandre", position: "right" },
+            { name: "Nitori", sprite: "nitori", position: "left" }
+        ]
+    },
+    {
+        speaker: "Nitori",
+        text: "hi! nice to meet you too",
+        background: "tangerang",
+        characters: [
+            { name: "Flandre", sprite: "flandre", position: "right" },
+            { name: "Nitori", sprite: "nitori", position: "left" }
+        ]
+    },
+    {
+        speaker: "Flandre",
+        text: "It's a nice day today",
+        background: "tangerang",
+        characters: [
+            { name: "Flandre", sprite: "flandre3", position: "right" },
+            { name: "Nitori", sprite: "nitori3", position: "left" }
+        ]
+    },
+    {
+        speaker: "Nitori",
+        text: "Yes pretty nice",
+        background: "tangerang",
+        characters: [
+            { name: "Flandre", sprite: "flandre2", position: "right" },
+            { name: "Nitori", sprite: "nitori2", position: "left" }
+        ]
+    },
+    {
+        speaker: "Flandre",
+        text: "Here too",
+        background: "shrine",
+        characters: [
+            { name: "Flandre", sprite: "flandre3", position: "right" },
+            { name: "Nitori", sprite: "nitori4", position: "left" }
+        ]
+    },
+    {
+        speaker: "Nitori",
+        text: "Yes",
+        background: "shrine",
+        characters: [
+            { name: "Flandre", sprite: "flandre", position: "right" },
+            { name: "Nitori", sprite: "nitori2", position: "left" }
+        ]
+    },
+],
+
+};
--- src/components/VN/imageMap.js
+++ src/components/VN/imageMap.js
@@ -10,9 +10,24 @@ import sakuya2 from "../../assets/image/Characters/spriteSakuya/2.png";
 import sakuya3 from "../../assets/image/Characters/spriteSakuya/3.png";
 import sakuya4 from "../../assets/image/Characters/spriteSakuya/4.png";
 
+//Sprite Flandre
+import flandre1 from "../../assets/image/Characters/spriteFlandre/1.png";
+import flandre2 from "../../assets/image/Characters/spriteFlandre/2.png";
+import flandre3 from "../../assets/image/Characters/spriteFlandre/3.png";
+import flandre4 from "../../assets/image/Characters/spriteFlandre/4.png";
+
+//Sprite Nitori
+import nitori1 from "../../assets/image/Characters/spriteNitori/1.png";
+import nitori2 from "../../assets/image/Characters/spriteNitori/2.png";
+import nitori3 from "../../assets/image/Characters/spriteNitori/3.png";
+import nitori4 from "../../assets/image/Characters/spriteNitori/4.png";
+
 //Background
 import blokM from "../../assets/image/vn/blokM.png";
 import shrineBG from "../../assets/image/vn/shrine.jpg";
+import houseNoon from "../../assets/image/vn/HouseNoon.png";
+import tangerang from "../../assets/image/vn/tangerang.png";
+import tangNoon from "../../assets/image/vn/tangerangNoon.png";
 import houseNoon from "../../assets/image/vn/HouseNoon.png";
 
 export const characterSprites = {
@@ -24,10 +39,23 @@ export const characterSprites = {
     sakuya3: sakuya3,
     reimu4: reimu4,
     sakuya4: sakuya4,
+
+    flandre: flandre1,
+    flandre2: flandre2,
+    flandre3: flandre3,
+    flandre4: flandre4,
+
+    nitori: nitori1,
+    nitori2: nitori2,
+    nitori3: nitori3,
+    nitori4: nitori4,
 };
 
 export const backgroundImages = {
     blokM: blokM,
     shrine: shrineBG,
     house: houseNoon,
+    tangerang: tangerang,
+    tangNoon: tangNoon,
+    houseNoon: houseNoon,
 };
